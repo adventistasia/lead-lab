a629437bf374824aa911bffb83af6a59781fb0f8
agnojf <agnojf@gmail.com>
2026-09-09 15:17:18 +0800

    feat: add dark theme logo

--- application/source/resources/js/components/app-logo-image.tsx
+++ application/source/resources/js/components/app-logo-image.tsx
@@ -1,10 +1,27 @@
 import type { ImgHTMLAttributes } from 'react';
+import { cn } from '@/lib/utils';
 
 type AppLogoImageProps = Omit<ImgHTMLAttributes<HTMLImageElement>, 'src'>;
 
 export default function AppLogoImage({
     alt = 'Lead Hub',
+    className,
     ...props
 }: AppLogoImageProps) {
-    return <img src="/lead-hub-logo.png" alt={alt} {...props} />;
+    return (
+        <>
+            <img
+                src="/lead-hub-logo.png"
+                alt={alt}
+                className={cn(className, 'dark:hidden')}
+                {...props}
+            />
+            <img
+                src="/lead-hub-logo-dark.png"
+                alt={alt}
+                className={cn(className, 'hidden dark:block')}
+                {...props}
+            />
+        </>
+    );
 }
